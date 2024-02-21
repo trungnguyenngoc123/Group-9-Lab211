015a6b31b52d481133cfcb417899ed5ef2a6ef46
trungnguyenngoc123 <137521550+trungnguyenngoc123@users.noreply.github.com>
2024-02-21 11:31:04 +0700

    Delete P0056 directory

--- P0056/src/Main.java
+++
@@ -1,8 +0,0 @@
-
-import Controller.Control;
-public class Main {
-    public static void main(String []args){
-        Control cl = new Control();
-        cl.run();
-    }
-}
--- P0056/src/View/View.java
+++
@@ -1,8 +0,0 @@
-
-package View;
-
-public class View {
-    public void displayMess(String mess){
-        System.out.print(mess);
-    }
-}
